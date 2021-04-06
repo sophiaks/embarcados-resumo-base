ad84215f76cd12f7af9d358995f537211ebfbad9
Sophia <sophia.shigueoka@gmail.com>
2021-04-05 23:08:34 -0300

    Testando

---
+++ template.c
@@ -0,0 +1,67 @@
+// LEDs
+#define LED_PIO        PIOC
+#define LED_PIO_ID        ID_PIOC
+#define LED_PIO_IDX       8
+#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)
+
+// Bot�o - SAME70
+#define BUT_PIO         PIOA
+#define BUT_PIO_ID        ID_PIOA
+#define BUT_PIO_IDX       11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
+
+// Botoes - PIO (Ja pronto pra usar - 4 botoes externos)
+
+// EXT1 -- Primeiros 4 PIOs
+#define BUT1_PIO         PIOC
+#define BUT1_PIO_ID        ID_PIOC
+#define BUT1_PIO_IDX       31
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+#define BUT2_PIO         PIOA
+#define BUT2_PIO_ID        ID_PIOA
+#define BUT2_PIO_IDX       19
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX)
+
+#define BUT3_PIO         PIOB
+#define BUT3_PIO_ID        ID_PIOB
+#define BUT3_PIO_IDX       3
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
+
+#define BUT4_PIO         PIOB
+#define BUT4_PIO_ID        ID_PIOB
+#define BUT4_PIO_IDX       2
+#define BUT4_PIO_IDX_MASK (1u << BUT4_PIO_IDX)
+
+// EXT2 -- Primeiros 4 PIOs
+#define BUT1_PIO         PIOD
+#define BUT1_PIO_ID        ID_PIOD
+#define BUT1_PIO_IDX       30
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+#define BUT2_PIO         PIOC
+#define BUT2_PIO_ID        ID_PIOC
+#define BUT2_PIO_IDX       13
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX)
+
+#define BUT3_PIO         PIOA
+#define BUT3_PIO_ID        ID_PIOA
+#define BUT3_PIO_IDX       6
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
+
+#define BUT4_PIO         PIOD
+#define BUT4_PIO_ID        ID_PIOD
+#define BUT4_PIO_IDX       11
+#define BUT4_PIO_IDX_MASK (1u << BUT4_PIO_IDX)
+
+// LEDs - OLED
+
+// LED1 - PINO 7
+#define LED_PIO        PIOC
+#define LED_PIO_ID        ID_PIOC
+#define LED_PIO_IDX       8
+#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)
+ 
+// LED2 - PINO 8
+
+// LED3 - PINO 6
